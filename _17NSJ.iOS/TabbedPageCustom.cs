0b3b39f2cb0ecd3f81f137febdd6af3a0516c906
07JP27 <jynpei.t@gmail.com>
2017-11-14 20:53:49 +0900

    iOSタブ用カスタムレンダラーの実装

---
+++ _17NSJ.iOS/TabbedPageCustom.cs
@@ -0,0 +1,18 @@
+﻿using _17NSJ;
+using _17NSJ.iOS;
+using _17NSJ.Views;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: Xamarin.Forms.ExportRenderer(typeof(TabBaseView), typeof(TabbedPageCustom))]
+namespace _17NSJ.iOS
+{
+    public class TabbedPageCustom : TabbedRenderer
+    {
+        public TabbedPageCustom()
+        {
+            TabBar.UnselectedItemTintColor = new UIKit.UIColor(0.70f, 0.70f, 0.70f, 1.0f);
+            TabBar.TintColor = UIKit.UIColor.White;
+            TabBar.BarTintColor = new UIKit.UIColor(0.00f, 0.44f, 0.74f, 1.0f);
+        }
+    }
+}
