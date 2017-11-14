1dcd1c5777c06679fe0595f4e08e1c6f97fff8c8
07JP27 <jynpei.t@gmail.com>
2017-11-14 23:04:14 +0900

    タブの実装とホーム画面の基礎作成

--- _17NSJ.iOS/TabbedPageCustom.cs
+++ _17NSJ.iOS/TabbedPageCustom.cs
@@ -1,6 +1,7 @@
 ﻿using _17NSJ;
 using _17NSJ.iOS;
 using _17NSJ.Views;
+using UIKit;
 using Xamarin.Forms.Platform.iOS;
 
 [assembly: Xamarin.Forms.ExportRenderer(typeof(TabBaseView), typeof(TabbedPageCustom))]
@@ -10,9 +11,18 @@ namespace _17NSJ.iOS
     {
         public TabbedPageCustom()
         {
-            TabBar.UnselectedItemTintColor = new UIKit.UIColor(0.70f, 0.70f, 0.70f, 1.0f);
-            TabBar.TintColor = UIKit.UIColor.White;
+            TabBar.UnselectedItemTintColor = UIKit.UIColor.White;
+            TabBar.TintColor = new UIKit.UIColor(0.97f, 0.58f, 0.12f, 1.0f);
             TabBar.BarTintColor = new UIKit.UIColor(0.00f, 0.44f, 0.74f, 1.0f);
+
+            if (TabBar.Items != null)
+            {
+                foreach (var t in TabBar.Items)
+                {
+                    var fontSize = new UITextAttributes() { Font = UIFont.BoldSystemFontOfSize(15)  };
+                    t.SetTitleTextAttributes(fontSize, UIControlState.Normal);
+                }
+            }
         }
     }
 }
