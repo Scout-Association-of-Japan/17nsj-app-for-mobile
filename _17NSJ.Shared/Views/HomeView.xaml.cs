060e4cf970b9577218a1bd1530e7debf20c3f121
07JP27 <jynpei.t@gmail.com>
2017-11-15 23:10:06 +0900

    home画面内での画面遷移のテスト実装

--- _17NSJ.Shared/Views/HomeView.xaml.cs
+++ _17NSJ.Shared/Views/HomeView.xaml.cs
@@ -10,6 +10,12 @@ namespace _17NSJ.Views
         public HomeView()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        void DocumentClicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new _17NSJPage(), true);
         }
     }
 }
