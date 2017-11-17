9fd68e80eca95d70ef673be1cd0bb6cb69ad57e2
07JP27 <jynpei.t@gmail.com>
2017-11-17 22:38:08 +0900

    Home画面のイベント実装

--- _17NSJ.Shared/Views/HomeView.xaml.cs
+++ _17NSJ.Shared/Views/HomeView.xaml.cs
@@ -13,13 +13,41 @@ namespace _17NSJ.Views
             NavigationPage.SetHasNavigationBar(this, false);
         }
 
-        void DocumentClicked(object sender, System.EventArgs e)
+        void DocumentClicked(object sender, EventArgs e)
         {
         }
 
-        void SocialClicked(object sender, System.EventArgs e)
+        void SocialClicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new SocialView(), true);
+        }
+
+        void DayVisitorClicked(object sender, EventArgs e)
+        {
+        }
+
+        void MovieClicked(object sender, EventArgs e)
+        {
+        }
+
+        void ShopClicked(object sender, EventArgs e)
+        {
+        }
+
+        void SponsorClicked(object sender, EventArgs e)
+        {
+        }
+
+        void NewspaperClicked(object sender, EventArgs e)
+        {
+        }
+
+        void FriendShipClicked(object sender, EventArgs e)
+        {
+        }
+
+        void SettingClicked(object sender, EventArgs e)
+        {
         }
     }
 }
