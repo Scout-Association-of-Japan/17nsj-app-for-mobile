85e8349e2fd00aeca80fd8d39b5954559f2527e7
07JP27 <jynpei.t@gmail.com>
2017-11-24 22:24:15 +0900

    へこむボタンコントロールの実装

---
+++ _17NSJ.Shared/Controls/CollapseButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace _17NSJ.Controls
+{
+    public class CollapseButton : Button
+    {
+        public CollapseButton(): base()
+        {
+            const int _animationTime = 100;
+
+            Pressed += async (sender, e) =>
+            {
+                var btn = sender as CollapseButton;
+
+                if (btn != null)
+                {
+                    await btn.ScaleTo(0.5, _animationTime);
+                    await btn.ScaleTo(1, _animationTime);
+                }
+            };
+        }
+    }
+}
