4d52f2211a6d5638eee91ba58afc79a23b96617a
07JP27 <jynpei.t@gmail.com>
2017-11-26 14:01:35 +0900

    Friendship画面の実装

---
+++ _17NSJ.Shared/Constants/FriendshipList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using _17NSJ.Models;
+
+namespace _17NSJ.Constants
+{
+    public static class FriendshipList
+    {
+        static FriendshipList()
+        {
+            ObservableCollection<Friendship> list = new ObservableCollection<Friendship>();
+            list.Add(new Friendship() { Title = "大会全般", HashTag = "#17NSJ", ImagePath = "friendship_twitter.png", Url = "https://twitter.com/search?f=tweets&vertical=default&q=%2317NSJ&src=typd" });
+            list.Add(new Friendship() { Title = "交流会など", HashTag = "#17NSJ_Friendship", ImagePath = "friendship_twitter.png", Url = "https://twitter.com/search?f=tweets&q=%2317NSJ_Friendship&src=typd" });
+            List = list;
+        }
+
+        public static readonly ObservableCollection<Friendship> List;
+
+    }
+}
--- _17NSJ.Shared/Views/FriendShipView.xaml.cs
+++ _17NSJ.Shared/Views/FriendShipView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using _17NSJ.Constants;
+using _17NSJ.Models;
 using Xamarin.Forms;
 
 namespace _17NSJ.Views
@@ -10,6 +11,17 @@ namespace _17NSJ.Views
         public FriendShipView()
         {
             InitializeComponent();
+            this.friendshiplList.ItemsSource = FriendshipList.List;
+        }
+
+        void ItemSelected(object sender, ItemTappedEventArgs e)
+        {
+            var item = e.Item as Friendship;
+
+            if(item != null)
+            {
+                Device.OpenUri(new Uri(item.Url));
+            }
         }
     }
 }
