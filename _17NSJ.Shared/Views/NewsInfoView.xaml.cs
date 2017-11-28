3952ba32cfab3d88fe1372a8813ad1a447366402
07JP27 <jynpei.t@gmail.com>
2017-11-28 22:46:36 +0900

    newsinfo画面のレイアウト実装

--- _17NSJ.Shared/Views/NewsInfoView.xaml.cs
+++ _17NSJ.Shared/Views/NewsInfoView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using _17NSJ.Models;
 
 using Xamarin.Forms;
 
@@ -10,6 +12,26 @@ namespace _17NSJ.Views
         public NewsInfoView()
         {
             InitializeComponent();
+
+            var list = new ObservableCollection<NewsInfo>
+            {
+                new NewsInfo() { Color="#2e8b57", Title="タイトルこんにちはタイトルこんにちはああ", ThumbnailURL="https://pbs.twimg.com/profile_images/898696996814376960/9ObeFpCx_400x400.jpg", Outline="アウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトライン"},
+                new NewsInfo() { Color="#00008b", Title="タイトル2", ThumbnailURL="https://pbs.twimg.com/profile_images/898696996814376960/9ObeFpCx_400x400.jpg", Outline="アウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトライン"},
+                new NewsInfo() { Color="#ff0000", Title="タイトル3", ThumbnailURL="https://pbs.twimg.com/profile_images/898696996814376960/9ObeFpCx_400x400.jpg", Outline="アウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトライン"},
+                new NewsInfo() { Color="#32cd32", Title="タイトル4", ThumbnailURL="https://pbs.twimg.com/profile_images/898696996814376960/9ObeFpCx_400x400.jpg", Outline="アウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトラインアウトライン"}
+            };
+
+            this.newsInfoList.ItemsSource = list;
+        }
+
+        void ItemSelected(object sender, ItemTappedEventArgs e)
+        {
+            var item = e.Item as Social;
+
+            if (item != null)
+            {
+                Device.OpenUri(new Uri(item.Url));
+            }
         }
     }
 }
