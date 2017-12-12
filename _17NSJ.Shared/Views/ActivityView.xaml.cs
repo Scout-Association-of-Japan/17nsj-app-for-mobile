575dc86faea100b91ea7b5fd2bd8b91220f9a585
07JP27 <jynpei.t@gmail.com>
2017-12-12 22:17:20 +0900

    ActivityViewの仮実装

--- _17NSJ.Shared/Views/ActivityView.xaml.cs
+++ _17NSJ.Shared/Views/ActivityView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using _17NSJ.Models;
+using _17NSJ.Services;
 using Xamarin.Forms;
 
 namespace _17NSJ.Views
@@ -10,6 +12,38 @@ namespace _17NSJ.Views
         public ActivityView()
         {
             InitializeComponent();
+            GetActivitiesAsync();
+        }
+
+        private async void GetActivitiesAsync()
+        {
+            this.indicator.IsVisible = true;
+
+            var service = new AppDataService();
+            var actList = await service.GetActivitiesAsync();
+
+            //TODO リリース時削除
+            await Task.Delay(2000);
+
+            this.activityList.ItemsSource = actList;
+
+            this.activityList.EndRefresh();
+            this.indicator.IsVisible = false;
+        }
+
+        private void ListPulled(object sender, System.EventArgs e)
+        {
+            GetActivitiesAsync();
+        }
+
+        private void ItemSelected(object sender, ItemTappedEventArgs e)
+        {
+            var act = e.Item as ActivityModel;
+
+            if (act != null)
+            {
+                
+            }
         }
     }
 }
