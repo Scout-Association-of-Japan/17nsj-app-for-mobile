f80e5fba86ccd616feeeea0aba22df4f174cd87b
07JP27 <jynpei.t@gmail.com>
2017-12-27 23:20:42 +0900

    利用規約画面の実装

--- _17NSJ.Shared/Views/Agreement.xaml.cs
+++ _17NSJ.Shared/Views/Agreement.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using _17NSJ.Constants;
 using Xamarin.Forms;
 
 namespace _17NSJ.Views
@@ -10,6 +10,17 @@ namespace _17NSJ.Views
         public Agreement()
         {
             InitializeComponent();
+            this.BeforeArticle.Text = AgreementArticles.BeforeArticle;
+            this.Article1.Text = AgreementArticles.Article1;
+            this.Article2.Text = AgreementArticles.Article2;
+            this.Article3.Text = AgreementArticles.Article3;
+            this.Article4.Text = AgreementArticles.Article4;
+            this.Article5.Text = AgreementArticles.Article5;
+            this.Article6.Text = AgreementArticles.Article6;
+            this.Article7.Text = AgreementArticles.Article7;
+            this.Article8.Text = AgreementArticles.Article8;
+            this.Article9.Text = AgreementArticles.Article9;
+            this.Article10.Text = AgreementArticles.Article10;
         }
     }
 }
