ade0bfb1d9db8a04cd92e235f4aa9957efdf2356
07JP27 <jynpei.t@gmail.com>
2017-12-28 21:51:19 +0900

    プライバシーポリシー画面の実装

--- _17NSJ.Shared/Views/Policy.xaml.cs
+++ _17NSJ.Shared/Views/Policy.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using _17NSJ.Constants;
 
 using Xamarin.Forms;
 
@@ -10,6 +11,18 @@ namespace _17NSJ.Views
         public Policy()
         {
             InitializeComponent();
+            this.Article1.Text = PolicyArticles.Article1;
+            this.Article2.Text = PolicyArticles.Article2;
+            this.Article3.Text = PolicyArticles.Article3;
+            this.Article4.Text = PolicyArticles.Article4;
+            this.Article5.Text = PolicyArticles.Article5;
+            this.Article6.Text = PolicyArticles.Article6;
+            this.Article7.Text = PolicyArticles.Article7;
+            this.Article8.Text = PolicyArticles.Article8;
+            this.Article9.Text = PolicyArticles.Article9;
+            this.Article10.Text = PolicyArticles.Article10;
+            this.Article11.Text = PolicyArticles.Article11;
+            this.Article12.Text = PolicyArticles.Article12;
         }
     }
 }
