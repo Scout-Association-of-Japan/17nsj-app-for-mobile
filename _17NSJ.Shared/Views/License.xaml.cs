7cc58b169ab983483205ca5be4160a63b6a73328
07JP27 <jynpei.t@gmail.com>
2017-12-31 12:10:51 +0900

    ライセンス情報の更新

--- _17NSJ.Shared/Views/License.xaml.cs
+++ _17NSJ.Shared/Views/License.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace _17NSJ.Views
@@ -10,6 +10,12 @@ namespace _17NSJ.Views
         public License()
         {
             InitializeComponent();
+            ObservableCollection<string> list = new ObservableCollection<string>();
+            list.Add("Xamarin.Forms");
+            list.Add("Newtonsoft.Json");
+            list.Add("AiForms.Layouts");
+            list.Add("Com.Airbnb.Xamarin.Forms.Lottie");
+            this.licenseList.ItemsSource = list;
         }
     }
 }
