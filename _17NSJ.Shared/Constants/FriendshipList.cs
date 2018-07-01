ed04581a5d0d3c8e143af56c7e2cb8f261bdc0b5
07JP27 <jynpei.t@gmail.com>
2018-07-01 21:59:32 +0900

    ハッシュタグの追加

--- _17NSJ.Shared/Constants/FriendshipList.cs
+++ _17NSJ.Shared/Constants/FriendshipList.cs
@@ -12,6 +12,8 @@ namespace _17NSJ.Constants
             list.Add(new FriendshipModel() { Title = "大会全般", HashTag = "#17NSJ", ImagePath = "friendship_twitter.png", Url = "https://twitter.com/search?f=tweets&vertical=default&q=%2317NSJ&src=typd" });
             list.Add(new FriendshipModel() { Title = "交流会募集", HashTag = "#17NSJ_Friendship", ImagePath = "friendship_twitter.png", Url = "https://twitter.com/search?f=tweets&q=%2317NSJ_Friendship&src=typd" });
             list.Add(new FriendshipModel() { Title = "プログラム待ち時間共有", HashTag = "#17NSJ_WaitingTime", ImagePath = "friendship_twitter.png", Url = "https://twitter.com/search?f=tweets&q=%2317NSJ_WaitingTime&src=typd" });
+            list.Add(new FriendshipModel() { Title = "広報部への取材依頼", HashTag = "#17NSJ_PR", ImagePath = "friendship_twitter.png", Url = "https://twitter.com/search?f=tweets&q=%2317NSJ_PR&src=typd" });
+            list.Add(new FriendshipModel() { Title = "スカウト通信員が発信中", HashTag = "#スカウト通信員", ImagePath = "friendship_twitter.png", Url = "https://twitter.com/search?f=tweets&q=%23%E3%82%B9%E3%82%AB%E3%82%A6%E3%83%88%E9%80%9A%E4%BF%A1%E5%93%A1&src=typd" });
             List = list;
         }
 
